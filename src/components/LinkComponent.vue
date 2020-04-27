14cbb162547c634753c7cfe30706f6589bb01e00
Alan Dell'Onze <a.dellonze@techgap.it>
2020-04-27 20:48:51 +0200

    fix LinkComponent page

--- src/components/LinkComponent.vue
+++ src/components/LinkComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="linkComponent" v-if="band != null">
-    <p class="copyToClipboard" @click="copyToClipboard(band.id)">{{band.id}}</p>
+    <p class="copyToClipboard" @click="copyToClipboard(band.id)">band id: {{band.id}}</p>
     <p><a :href="buildLink()" target="_blank">{{buildLink()}}</a></p>
   </div>
 </template>
@@ -16,17 +16,13 @@ export default {
   },
 
   methods: {
+    copyToClipboard (text) {
+      navigator.clipboard.writeText(text)
+    },
+
     buildLink () {
       let url = process.env.VUE_APP_MA_METAL_ARCHIVES_URL
-      return url.replace('%s', this.band.id)
-    },
-
-    async copyToClipboard (bandId) {
-      await navigator.clipboard.writeText(bandId)
-      this.highlightElement(bandId)
-    },
-
-    highlightElement (bandId) {
+      return url.replace('%s', this.band.maKey)
     }
   }
 }
